7525af8fc88fd0e5b041410dc05e715c3fcb1ba6
Kostiantyn Servetnyk <78149855+kistyak@users.noreply.github.com>
2022-09-22 19:02:43 +0300

    Implemented writing posts, add it to db and show their

--- src/main/java/net/kistyak/site/controllers/BlogController.java
+++ src/main/java/net/kistyak/site/controllers/BlogController.java
@@ -6,6 +6,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.ArrayList;
+import java.util.Optional;
 
 @Controller
 public class BlogController{
@@ -20,4 +26,30 @@ public class BlogController{
         return "blog";
     }
 
+    @GetMapping("/blog/new")
+    public String blogNew(Model model) {
+        return "new";
+    }
+
+    @PostMapping("blog/new")
+    public String blogNewPost(@RequestParam String title,
+                              @RequestParam String anons,
+                              @RequestParam String full_text,
+                              Model model){
+        Post post = new Post(title, anons, full_text);
+        postRepository.save(post);
+        return "redirect:/blog";
+    }
+
+    @GetMapping("/blog/{id}")
+    public String blogShow(@PathVariable(value = "id") long id, Model model) {
+        if(!postRepository.existsById(id)){
+            return "redirect:/blog";
+        }
+        Optional<Post> post = postRepository.findById(id);
+        ArrayList<Post> result = new ArrayList<>();
+        post.ifPresent(result::add);
+        model.addAttribute("post", result);
+        return "show";
+    }
 }
